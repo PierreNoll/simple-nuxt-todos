a765f7a01a4d2c6091f76bbdaf6aca7e66e9a29c
NOLL Pierre <Pierre.NOLL@egis.fr>
2019-03-05 22:10:10 +0100

    basic layout : navbar + footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
 <v-app light>
+  <v-toolbar :app="true" :light="true">
+    <v-toolbar-title>Simple Nuxt Todo</v-toolbar-title>
+  </v-toolbar>
+  <v-content>
+    <v-container>
+      <nuxt />
+    </v-container>
+  </v-content>
 
+  <v-footer>
+    <span>&copy; 2019</span>
+  </v-footer>
 </v-app>
 </template>
 
