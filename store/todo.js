dcfc0102c2a2f3acf725a68915a6c6a21ed3f687
NOLL Pierre <Pierre.NOLL@egis.fr>
2019-03-06 00:11:28 +0100

    add todo action

--- store/todo.js
+++ store/todo.js
@@ -7,6 +7,9 @@ export const state = () => {
 export const mutations = {
   SET_TODOS(state, todos) {
     state.todos = todos
+  },
+  ADD_TODO(state, todo) {
+    state.todos.push(todo)
   }
 }
 
@@ -14,5 +17,20 @@ export const actions = {
   async fetchTodos({ commit }) {
     const todos = await this.$axios.$get('http://localhost:3001/todos')
     commit('SET_TODOS', todos)
+  },
+  async createTodo({ commit }, todo) {
+    const id = Math.floor(Math.random() * 10000000)
+    try {
+      await this.$axios.$post('http://localhost:3001/todos', {
+        ...todo,
+        id: id
+      })
+    } catch (e) {
+      throw e
+    }
+    commit('ADD_TODO', {
+      ...todo,
+      id: id
+    })
   }
 }
