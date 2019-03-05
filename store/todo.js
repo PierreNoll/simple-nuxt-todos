132d57bb82ce05a9ecbb7a3acad6c87751bd8b8a
NOLL Pierre <Pierre.NOLL@egis.fr>
2019-03-05 22:54:43 +0100

    add json server globally that watches db.json

--- store/todo.js
+++ store/todo.js
@@ -1,22 +1,6 @@
 export const state = () => {
   return {
-    todos: [
-      {
-        id: 1,
-        value: 'faire les courses',
-        status: 'not-done'
-      },
-      {
-        id: 2,
-        value: 'faire à manger',
-        status: 'not-done'
-      },
-      {
-        id: 3,
-        value: 'commencer le test clementine',
-        status: 'done'
-      }
-    ]
+    todos: []
   }
 }
 
